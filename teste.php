f95fe7c950dacbf2cb0901a36f9aae7b48816ff0
evelynoliveira <evelyn.oliveira@-era.br.com>
2020-07-06 19:38:33 -0300

    Refactor: teste.php

--- teste.php
+++ teste.php
@@ -2,5 +2,14 @@
     require_once 'DAO/perfisDAO.php';
 
     $meuPerfil = new perfisDAO();
-    //$meuArray = $meuPerfil->load();
+    $meuArray = $meuPerfil->load();
+    
+    ///print_r($meuArray);
+    var_dump($meuArray);
+    //echo "<h1>" . $meuArray . "</h1>";
+    foreach($meuArray as $value) {
+        echo '<pre>'; 
+        print_r($value->nome);
+        echo '<br>';
+    }
 ?>

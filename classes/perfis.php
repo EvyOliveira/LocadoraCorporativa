751d660994d8ac5df35e4e656cef4aa94be3b7a8
evelynoliveira <evelyn.oliveira@-era.br.com>
2020-07-06 20:11:30 -0300

    Refactor: perfis.php

--- classes/perfis.php
+++ classes/perfis.php
@@ -1,19 +1,58 @@
 <?php
-	class perfis {
-		private $id;
-		private $nome;			
-		public function getId(){
-			return $this->id;
-		}		
-		public function getNome(){
-			return $this->nome;
-		}
-			
-		public function setId($id){
-			$this->id = $id;
-		}		
-		public function setNome($nome){
-			$this->nome = $nome;
-		}		
+require_once ($_SERVER['DOCUMENT_ROOT'] . 'login_DAO/DAO/Crud.php');
+session_start();
+
+class usuarios extends Crud {
+	protected $table = 'usuarios';
+	private $id;
+	private $nome;
+	private $email;
+	private $senha;
+	private $perfil;
+
+	public function insert(){}
+	public function update(){}
+	public function autenticacao(){}
+	
+	public function getId(){
+		return $this->id;
+	}
+	
+	public function getNome(){
+		return $this->nome;
+	}
+	
+	public function getEmail(){
+		return $this->email;
+	}
+	
+	public function getSenha(){
+		return $this->senha;
+	}
+
+	public function getPerfil(){
+		return $this->perfil;
+	}
+	
+	public function setId($id){
+		$this->id = $id;
+	}
+	
+	public function setNome($nome){
+		$this->nome = $nome;
+	}
+	
+	public function setEmail($email){
+		$this->email = $email;
+	}
+	
+	public function setPerfil($perfil){
+		$this->perfil = $perfil;
+	}
+
+	public function setSenha($senha){
+		$this->senha = md5($senha);		
+	}		
 }
+
 ?>

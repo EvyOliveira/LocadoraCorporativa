389e679c5f77d68fa582223a50963b1a8ba8a9bb
evelynoliveira <evelyn.oliveira@-era.br.com>
2020-07-06 20:04:13 -0300

    Refactor: perfisDAO.php

--- DAO/perfisDAO.php
+++ DAO/perfisDAO.php
@@ -1,18 +1,15 @@
 <?php
-    require_once($_SERVER['DOCUMENT_ROOT'].'/classes/perfis.php');
-    //require_once 'classes/perfis.php';
-    require_once 'Crud.php';
+    //require_once '$_SERVER['DOCUMENT_ROOT'].'classes/perfis.php';
+    require_once ($_SERVER['DOCUMENT_ROOT'] . 'login_DAO/classes/perfis.php');
 
     class perfisDAO extends Crud {
         private $d_perfil;
         protected $table = 'perfis';
-        //Construtor
-        public function __construct(){
+        public function __construct($p_perfis){
+            $this->d_perfil = $p_perfis;
         }
-        //Clone
         public function __clone(){
         }
-        //Destrutor
         public function __destruct(){
             foreach($this as $key => $value):
                 unset($this->$key);
@@ -21,28 +18,19 @@
                 unset(${"$var"});
             endforeach;
         }
-
         public function insert(){
-    		$sql  = "INSERT INTO $this->table (nome) VALUES ('". $this->d_perfil->nome . "')";
-    		$stmt = DB::prepare($sql);
-    		return $stmt->execute(); 
         }
-        
-        public function update($id){
-    		$sql  = "UPDATE $this->table SET nome = '".$this->d_perfil->nome."' WHERE id = this->d_perfil->id";
-    		$stmt = DB::prepare($sql);
-    		return $stmt->execute();	
-    	}
-        
-        //public function autenticacao(){}
-
+        public function update(){
+        }
+        public function autenticacao(){
+        }
         public function load(){
-    	    // Buscando todos os dados da tabela perfis
+            // Buscando todos os dados da tabela perfis
             $arr = $this->findAll();
             // Montando o array de objetos perfis
             foreach($arr as $chave => $valor){
                 $objeto = new perfis();
-                $objeto->seTId($valor->id);
+                $objeto->setId($valor->id);
                 $objeto->setNome($valor->nome);
                 $arrPerfis[] = $objeto;
             }

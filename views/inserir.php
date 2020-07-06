137bfbca63230927c803e16e11ee1d3e68972d2e
evelynoliveira <evelyn.oliveira@-era.br.com>
2020-07-06 19:47:46 -0300

    Add: inserir.php

---
+++ views/inserir.php
@@ -0,0 +1,12 @@
+<?php
+	require_once($_SERVER['DOCUMENT_ROOT'] . 'login_DAO/DAO/usuariosDAO.php');
+	
+	$myuser = new usuarios();
+	$myuser->setNome($_POST['f_nome']);
+	$myuser->setEmail($_POST['f_mail']);
+	$myuser->setSenha(md5($_POST['f_senha']));
+	$myuser->setPerfil($_POST['f_perfil']);
+	$myUser = new usuariosDAO($myuser);
+	$myUser->insert();
+	Header("Location:../views/cadastro.php");
+?>

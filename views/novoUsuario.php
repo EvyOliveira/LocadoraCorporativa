1ef1721414feb0d9cfb9bdb075b06b392a56fc51
evelynoliveira <evelyn.oliveira@-era.br.com>
2020-07-06 19:46:19 -0300

    Add: novoUsuario.php

--- views/novoUsuario.php
+++ views/novoUsuario.php
@@ -1,11 +1,12 @@
 <?php
-    include "../scripts/usuarios.php";
+	require_once ($_SERVER['DOCUMENT_ROOT'] . 'login_DAO/DAO/usuariosDAO.php');
+	require_once ($_SERVER['DOCUMENT_ROOT'] . 'login_DAO/DAO/perfisDAO.php');
 ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../css/reset.css">
-    <link rel="stylesheet" href="../css/style-home.css">   
+    <link rel="stylesheet" href="../css/style.css">   
     <title>Cadastro</title>
 </head>
 <body>
@@ -17,40 +18,55 @@
             <input type="text" name="f_mail" placeholder="Email"/>
             <br/>                    
             <input type="password" name="f_senha" id="txtPassword" placeholder="Senha"/>
-            <br/>
+            <br/>			
+			<?php 
+				echo "<select name='f_perfil' id='f_perfil'>";
+				$perfDAO = new perfisDAO();
+				$arr = $perfDAO->load();
+				foreach($arr as $perfis):
+					if($perfis->getId() == $_POST['f_perfil']){
+						echo("<option value =".$perfis->getId()." selected>".$perfis->getNome()."</option>");
+					}else{
+						echo("<option value =".$perfis->getId().">".$perfis->getNome()."</option>"); 
+					}
+				endforeach;
+				echo "</select>";
+			?>
             <button onclick="validar_cadastro()">Criar Conta</button>
             <br/><br/><br/>
-            <a href="../index.php" id="navegacao">Retornar à página inicial</a>
+            <a href="../index.php" id="navegacao">Retornar ao Login</a>
         </form>
     </div>
-    <?php
-			$myuser = new usuarios();
-			if(!empty($_POST['f_nome']) and !empty($_POST['f_mail']) and !empty($_POST['f_senha'])){
-                $myuser->setNome($_POST['f_nome']);
-				$myuser->setEmail($_POST['f_mail']);
-				$myuser->setSenha($_POST['f_senha']);
-				$myuser->insert();				
-			}
-			else {
-				echo "Todos os campos são obrigatórios!";
-			}
-		?>
-		<div>
-			<table border=1>
-				<tr>
-					<th>Nome</th>
-					<th>Email</th>
-				</tr>
-				
-				
-				<?php foreach($myuser->findAll() as $key=>$value): ?>
-				<tr>
-					<td><?php echo "$value->nome"; ?></td>
-					<td><?php echo "$value->email"; ?></td>
-				</tr>
-				<?php endforeach; ?>
-			</table>
-		</div>		
-		</b>
+	<?php
+		$myuser = new usuarios();
+		if(!empty($_POST['f_nome']) and !empty($_POST['f_mail']) and !empty($_POST['f_senha'])){
+			$myuser->setNome($_POST['f_nome']);
+			$myuser->setEmail($_POST['f_mail']);
+			$myuser->setSenha($_POST['f_senha']);
+			$myuser->setPerfil($_POST['f_perfil']);
+			$myuserDAO = new usuariosDAO($myuser);
+			$myuserDAO->insert();				
+		}
+		else {			
+			echo "Todos os campos são obrigatórios!";
+		}
+	?>
+	<div>
+		<table border=1>
+			<tr>
+				<th>Nome</th>
+				<th>Email</th>
+			</tr>
+			
+			
+			<?php foreach($myuser->findAll() as $key=>$value): ?>
+			<tr>
+				<td><?php echo "$value->nome"; ?></td>
+				<td><?php echo "$value->email"; ?></td>
+			</tr>
+			<?php endforeach; ?>
+		</table>
+	</div>		
+	</b>
 </body>
 </html>

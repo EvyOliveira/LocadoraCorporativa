b4560038f771144feab43f1cd9aff06f8d2a4a6c
evelynoliveira <evelyn.oliveira@-era.br.com>
2020-07-06 23:10:06 -0300

    Refactor: /classes/usuarios.php

--- classes/usuarios.php
+++ classes/usuarios.php
@@ -1,55 +1,57 @@
 <?php
+require_once ($_SERVER['DOCUMENT_ROOT'] . '/login_DAO/DAO/Crud.php');
 
-session_start();
-require_once 'Crud.php';
+class usuarios extends Crud {
+	protected $table = 'usuarios';
+	private $id;
+	private $nome;
+	private $email;
+	private $senha;
+	private $perfil;
 
-	class usuarios extends Crud {
-		protected $table = 'usuarios';
-		private $id;
-		private $nome;
-		private $email;
-		private $senha;
-		private $id_perfil;
-		
-		public function getId(){
-			return $this->id;
-		}
-		
-		public function getNome(){
-			return $this->nome;
-		}
-		
-		public function getEmail(){
-			return $this->email;
-		}
-		
-		public function getSenha(){
-			return $this->senha;
-		}
-
-		public function getPerfil(){
-			return $this->id_perfil;
-		}
-		
-		public function setId($id){
-			$this->id = $id;
-		}
-		
-		public function setNome($nome){
-			$this->nome = $nome;
-		}
-		
-		public function setEmail($email){
-			$this->email = $email;
-		}
-		
-		public function setSenha($senha){
-			$this->senha = md5($senha);
-		}
-
-		public function setPerfil($id_perfil){
-			$this->id_perfil = $id_perfil;
-		}
+	public function insert(){}
+	public function update(){}
+	public function autenticacao(){}
+	
+	public function getId(){
+		return $this->id;
 	}
+	
+	public function getNome(){
+		return $this->nome;
+	}
+	
+	public function getEmail(){
+		return $this->email;
+	}
+	
+	public function getSenha(){
+		return $this->senha;
+	}
+
+	public function getPerfil(){
+		return $this->perfil;
+	}
+	
+	public function setId($id){
+		$this->id = $id;
+	}
+	
+	public function setNome($nome){
+		$this->nome = $nome;
+	}
+	
+	public function setEmail($email){
+		$this->email = $email;
+	}
+	
+	public function setPerfil($perfil){
+		$this->perfil = $perfil;
+	}
+
+	public function setSenha($senha){
+		$this->senha = md5($senha);		
+	}		
+}
 
 ?>

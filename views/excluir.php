8b71864a4843857216c926d626b69b8149f54da7
evelynoliveira <evelyn.oliveira@-era.br.com>
2020-07-06 22:53:58 -0300

    Refactor: /views/excluir.php

--- views/excluir.php
+++ views/excluir.php
@@ -1,7 +1,10 @@
 <?php
-	require_once($_SERVER['DOCUMENT_ROOT'] . 'login_DAO/DAO/usuariosDAO.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/login_DAO/DAO/usuariosDAO.php');
 	
-	$myUser = new usuariosDAO();
-	$myUser->delete($_POST['f_id']);
-	Header("Location:../views/cadastro.php");
+	$myUser = new usuarios();
+	$myUserDAO = new usuariosDAO($myUser);
+	$myUserDAO->delete($_POST['f_id']);
+	//Header("Location:../views/cadastro.php");
+	$URL = "./cadastro.php";
+    echo "<script type='text/javascript'>document.location.href='{$URL}';</script>";
 ?>
